1a0e1e3d51c5833a4de1fce8e4c79c5e63acf7e5
JeffersonAguiar <jfdruida@gmail.com>
2024-06-18 14:19:22 +0100

    FEAT:  Update ALLOWED_HOSTS in settings.py to include .herokuapp.com

--- planpal/settings.py
+++ planpal/settings.py
@@ -28,10 +28,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [
+    ".herokuapp.com",
+]
 
 # Application definition
 
