b530dd707329374209c218989f1626e6894b988a
JeffersonAguiar <jfdruida@gmail.com>
2024-06-24 10:34:25 +0100

    Feat: Update logout URL in base.html

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path("", views.home, name="home"),
     path("dashboard/", views.dashboard, name="dashboard"),
+    path(
+        "logout/",
+        auth_views.LogoutView.as_view(template_name="account/logout.html"),
+        name="account_logout",
+    ),
 ]
