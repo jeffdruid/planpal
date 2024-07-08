b77c92e2730da96086f59a163ab6d86f0daf2f87
JeffersonAguiar <jfdruida@gmail.com>
2024-07-08 10:37:04 +0100

    refactor: Remove unused code for handling invitation responses

--- events/views.py
+++ events/views.py
@@ -9,7 +9,6 @@ from .utils import (
     notify_event_created,
     notify_event_updated,
     notify_event_cancelled,
-    notify_invitation_response,
     notify_suggested_alternate_date,
     notify_event_confirmed,
 )
@@ -74,10 +73,8 @@ def event_details(request, event_id):
         action = request.POST.get("action")
         if action == "accept":
             user_invitations.update(status="Accepted")
-            notify_invitation_response(user_invitations.first())
         elif action == "deny":
             user_invitations.update(status="Declined")
-            notify_invitation_response(user_invitations.first())
         elif action == "maybe":
             alternate_date = request.POST.get("alternate_date")
             alternate_time = request.POST.get("alternate_time")
