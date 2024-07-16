33e593bbf071fb9d9c8235470a00d35f50a572cb
JeffersonAguiar <jfdruida@gmail.com>
2024-07-16 10:16:30 +0100

    refactor: Add admin interface for managing Friendship model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
 from .models import UserProfile
+from .models import Friendship
 
 admin.site.register(UserProfile)
+# admin.site.register(Friendship)
+
+
+@admin.register(Friendship)
+class FriendshipAdmin(admin.ModelAdmin):
+    list_display = ("from_user", "to_user", "status", "created_at")
+    list_filter = ("status", "created_at")
+    search_fields = ("from_user__username", "to_user__username")
+    ordering = ("-created_at",)
