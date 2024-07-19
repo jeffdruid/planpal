8e421e23e948c42b30bb1970c2660503dba1202e
JeffersonAguiar <jfdruida@gmail.com>
2024-07-19 09:55:20 +0100

    refactor: Update modal dialog in dashboard.html for better accessibility and usability

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,4 +1,7 @@
-// Load the notification dropdown when the bell icon is clicked
+function openAlternateDateModal() {
+    $('#alternateDateModal').modal('show');
+}
+
 $(document).ready(function() {
     // Toggle notification dropdown
     $("#notificationBell").click(function(e) {
@@ -20,32 +23,58 @@ $(document).ready(function() {
             $(".dropdown-menu").hide();
         }
     });
+
+    // Event delete modal functionality
+    var deleteModal = $('#deleteModal');
+    var deleteForm = $('#deleteForm');
+
+    $('.delete-btn').click(function() {
+        var eventId = $(this).data('event-id');
+        deleteForm.attr('action', '/events/' + eventId + '/delete/');
+        deleteModal.modal('show');
+    });
+
+    $('#deleteModal .close, #deleteModal button[data-dismiss="modal"]').click(function() {
+        deleteModal.modal('hide');
+    });
+
+    // Delete friend modal functionality
+    var deleteFriendModal = $('#deleteFriendModal');
+    var deleteFriendForm = $('#deleteFriendForm');
+
+    $('.delete-friend-btn').click(function() {
+        var friendId = $(this).data('friend-id');
+        deleteFriendForm.attr('action', '/delete_friend/' + friendId + '/');
+        deleteFriendModal.modal('show');
+    });
+
+    $('#deleteFriendModal .close, #deleteFriendModal button[data-dismiss="modal"]').click(function() {
+        deleteFriendModal.modal('hide');
+    });
+
+    // Close modals when clicking outside
+    window.onclick = function(event) {
+        if (event.target == deleteModal[0]) {
+            deleteModal.modal('hide');
+        }
+        if (event.target == deleteFriendModal[0]) {
+            deleteFriendModal.modal('hide');
+        }
+        if (event.target == $('#alternateDateModal')[0]) {
+            $('#alternateDateModal').modal('hide');
+        }
+    };
+
+    // Function to close all modals
+    function closeModal() {
+        $('#deleteFriendModal').modal('hide');
+        $('#deleteModal').modal('hide');
+        $('#alternateDateModal').modal('hide');
+    }
+
+    // Alternate date form submission
+    $('#alternateDateForm').on('submit', function(event) {
+        event.preventDefault();
+        $(this).unbind('submit').submit();
+    });
 });
-
-// Modal handling functions
-function respondToInvitation(response) {
-    if (response === 'deny' || response === 'maybe') {
-        document.getElementById('alternateDateModal').style.display = 'block';
-    } else {
-        alert('Invitation accepted!');
-    }
-}
-
-function closeModal() {
-    document.getElementById('alternateDateModal').style.display = 'none';
-}
-
-// Close the modal if the user clicks outside of it
-window.onclick = function(event) {
-    if (event.target == document.getElementById('alternateDateModal')) {
-        closeModal();
-    }
-}
-
-// Handle the alternate date form submission
-document.getElementById('alternateDateForm').onsubmit = function(event) {
-    event.preventDefault();
-    let alternateDate = document.getElementById('alternate_date').value;
-    alert('Suggested alternate date: ' + alternateDate);
-    closeModal();
-}
