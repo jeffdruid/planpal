dc7de4e5a3dcd217485e1b1b2d909ba785f8d221
JeffersonAguiar <jfdruida@gmail.com>
2024-07-28 18:33:16 +0100

    refactor: Update event form templates to use separate JavaScript file

--- staticfiles/js/scripts.js
+++ staticfiles/js/scripts.js
@@ -38,19 +38,16 @@ function fetchNotifications() {
             let currentUnreadCount = data.unread_count;
 
             if (currentUnreadCount > previousUnreadCount) {
-                // alert("You have a new notification!");
                 console.log("New notification detected!");
                 updateNotifications(data.notifications, currentUnreadCount - previousUnreadCount);
             }
 
-            // Update the unread count badge
             let badge = $("#notificationBell .badge");
             badge.text(currentUnreadCount > 0 ? currentUnreadCount : '');
             if (currentUnreadCount > 0) {
                 badge.css('display', 'inline-block');
                 badge.css('opacity', 0);
                 badge.css('opacity', 1);
-                // badge.animate({opacity: 1}, 300);
             } else {
                 badge.css('display', 'none');
             }
@@ -59,7 +56,6 @@ function fetchNotifications() {
         },
         error: function(xhr, status, error) {
             console.error("Error fetching notifications:", xhr.status, xhr.statusText);
-            // console.log("Response:", xhr.responseText);
         }
     });
 }
@@ -68,19 +64,10 @@ function updateNotifications(notifications, newNotificationCount) {
     console.log("Updating notifications dropdown...");
     let dropdownMenu = $("#notificationDropdown");
 
-    // Remove "No notifications" if present
     dropdownMenu.find('a.dropdown-item:contains("No notifications")').remove();
 
-    // Add new notifications to the dropdown
     notifications.slice(0, newNotificationCount).forEach(function(notification) {
-        let notificationUrl;
-        if (notification.type === "event") {
-            notificationUrl = `/notifications/notifications/mark-read/${notification.id}/`;
-        } else if (notification.type === "friend_request_received") {
-            notificationUrl = `/notifications/notifications/mark-read/${notification.id}/`;
-        } else {
-            notificationUrl = `/notifications/notifications/mark-read/${notification.id}/`;
-        }
+        let notificationUrl = `/notifications/notifications/mark-read/${notification.id}/`;
 
         let notificationItem = `
             <a class="dropdown-item" href="${notificationUrl}">
@@ -90,12 +77,10 @@ function updateNotifications(notifications, newNotificationCount) {
         dropdownMenu.prepend(notificationItem);
     });
 
-    // Ensure "View All Notifications" is always present
     if (!dropdownMenu.find('a[href="/notifications/"]').length) {
         dropdownMenu.append('<div class="dropdown-divider"></div><a class="dropdown-item" href="/notifications/">View All Notifications</a>');
     }
 
-    // Handle display state when there are no notifications
     if (notifications.length === 0) {
         dropdownMenu.append('<a class="dropdown-item" href="#">No notifications</a>');
     }
@@ -111,15 +96,13 @@ $(document).ready(function() {
         fetchNotifications();
     });
 
-    // Toggle profile dropdown
     $("#profileLink").click(function(e) {
         e.preventDefault();
         console.log("Profile link clicked.");
         toggleDropdown("profileDropdown");
-        document.getElementById("notificationDropdown").style.display = 'none'; // Hide notification dropdown when profile dropdown is opened
+        document.getElementById("notificationDropdown").style.display = 'none';
     });
 
-    // Close dropdowns when clicking outside
     $(document).click(function(e) {
         if (!$(e.target).closest('.dropdown-menu, #notificationBell, #profileLink').length) {
             $(".dropdown-menu").each(function() {
@@ -132,7 +115,6 @@ $(document).ready(function() {
         }
     });
 
-    // FullCalendar setup
     if ($('#calendar').length) {
         $('#calendar').fullCalendar({
             header: {
@@ -144,7 +126,7 @@ $(document).ready(function() {
             timeZone: 'local',
             height: 'auto',
             contentHeight: 500,
-            events: window.calendarEvents, // Load events from a global variable
+            events: window.calendarEvents,
             timeFormat: 'h:mma',
             eventMouseover: function(event, jsEvent) {
                 var tooltip = '<div class="fc-tooltip">' +
@@ -163,7 +145,6 @@ $(document).ready(function() {
                     boxShadow: '0 2px 4px rgba(0,0,0,0.2)'
                 }).fadeIn('500').fadeTo('10', 1.9);
 
-                // Simulate a delay for loading content
                 setTimeout(function() {
                     $('.fc-tooltip').html(
                         '<strong>' + event.title + '</strong><br>' +
@@ -173,7 +154,7 @@ $(document).ready(function() {
                         'Description: ' + event.description + '<br>' +
                         'Created by: ' + event.creator
                     );
-                }, 1000);  // Adjust the delay as needed
+                }, 1000);
             },
             eventMouseout: function(event, jsEvent) {
                 $('.fc-tooltip').remove();
@@ -181,80 +162,21 @@ $(document).ready(function() {
         });
     }
 
-    // https://stackoverflow.com/questions/47710304/how-to-change-row-height-in-fullcalendar
-    
-    // Modify contentHeight based on window size
     function adjustCalendarContentHeight() {
         var contentHeight = $(window).width() < 992 ? 300 : 500;
         $('#calendar').fullCalendar('option', 'contentHeight', contentHeight);
     }
 
-    adjustCalendarContentHeight(); // Adjust on document ready
+    adjustCalendarContentHeight();
 
     $(window).resize(function() {
-        adjustCalendarContentHeight(); // Adjust on window resize
+        adjustCalendarContentHeight();
     });
 
-    // Event delete modal functionality
-    var deleteModal = $('#deleteModal');
-    var deleteForm = $('#deleteForm');
-
-    $('.delete-btn').click(function() {
-        var eventId = $(this).data('event-id');
-        deleteForm.attr('action', '/events/' + eventId + '/delete/');
-        deleteModal.modal('show');
-    });
-
-    $('#deleteModal .close, #deleteModal button[data-dismiss="modal"]').click(function() {
-        deleteModal.modal('hide');
-    });
-
-    // Delete friend modal functionality
-    var deleteFriendModal = $('#deleteFriendModal');
-    var deleteFriendForm = $('#deleteFriendForm');
-
-    $('.delete-friend-btn').click(function() {
-        var friendId = $(this).data('friend-id');
-        deleteFriendForm.attr('action', '/delete_friend/' + friendId + '/');
-        deleteFriendModal.modal('show');
-    });
-
-    $('#deleteFriendModal .close, #deleteFriendModal button[data-dismiss="modal"]').click(function() {
-        deleteFriendModal.modal('hide');
-    });
-
-    // Close modals when clicking outside
-    window.onclick = function(event) {
-        if (event.target == deleteModal[0]) {
-            deleteModal.modal('hide');
-        }
-        if (event.target == deleteFriendModal[0]) {
-            deleteFriendModal.modal('hide');
-        }
-        if (event.target == $('#alternateDateModal')[0]) {
-            $('#alternateDateModal').modal('hide');
-        }
-    };
-
-    // Function to close all modals
-    function closeModal() {
-        $('#deleteFriendModal').modal('hide');
-        $('#deleteModal').modal('hide');
-        $('#alternateDateModal').modal('hide');
-    }
-
-    // Alternate date form submission
-    $('#alternateDateForm').on('submit', function(event) {
-        event.preventDefault();
-        $(this).unbind('submit').submit();
-    });
-
-    // Fetch notifications every 10 seconds if user is authenticated
     if (userIsAuthenticated) {
         console.log("User is authenticated.");
         setInterval(fetchNotifications, 10000);
     } else {
         console.log("User is not authenticated.");
     }
-        
 });
