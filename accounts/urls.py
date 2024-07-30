206dea37ea3f59db02ee6ccb6b10cdce79bffc27
JeffersonAguiar <jfdruida@gmail.com>
2024-07-30 21:17:25 +0100

    refactor: Update password reset functionality and templates for improved user experience and security

--- accounts/urls.py
+++ accounts/urls.py
@@ -33,25 +33,12 @@ urlpatterns = [
         "view_profile/<int:user_id>/", views.view_profile, name="view_profile"
     ),
     path(
-        "password/reset/",
-        auth_views.PasswordResetView.as_view(
-            template_name="accounts/password_reset.html"
-        ),
-        name="password_reset",
+        "password/reset/", views.custom_password_reset, name="password_reset"
     ),
     path(
         "password_reset/done/",
-        auth_views.PasswordResetDoneView.as_view(
-            template_name="accounts/password_reset_done.html"
-        ),
+        views.password_reset_done,
         name="password_reset_done",
     ),
-    # path(
-    #     "reset/<uidb64>/<token>/",
-    #     auth_views.PasswordResetConfirmView.as_view(
-    #         template_name="registration/password_reset_confirm.html"
-    #     ),
-    #     name="password_reset_confirm",
-    # ),
     path("delete_account/", views.delete_account, name="delete_account"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -14,6 +14,47 @@ from notifications.models import Notification
 from django.urls import reverse
 from django.views.decorators.cache import cache_control
 
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.conf import settings
+from django.contrib.sites.shortcuts import get_current_site
+
+
+def custom_password_reset(request):
+    if request.method == "POST":
+        email = request.POST["email"]
+        try:
+            user = User.objects.get(email=email)
+            token = default_token_generator.make_token(user)
+            uid = urlsafe_base64_encode(force_bytes(user.pk))
+            current_site = get_current_site(request)
+            mail_subject = "Reset your password"
+            message = render_to_string(
+                "accounts/password_reset_email.html",
+                {
+                    "user": user,
+                    "domain": current_site.domain,
+                    "uid": uid,
+                    "token": token,
+                },
+            )
+            send_mail(
+                mail_subject, message, settings.DEFAULT_FROM_EMAIL, [email]
+            )
+            messages.success(request, "Password reset email has been sent.")
+            return redirect("password_reset_done")
+        except User.DoesNotExist:
+            messages.error(request, "No user is associated with this email.")
+            return redirect("password_reset")
+    return render(request, "accounts/password_reset.html")
+
+
+def password_reset_done(request):
+    return render(request, "accounts/password_reset_done.html")
+
 
 @login_required
 @cache_control(no_cache=True, must_revalidate=True, no_store=True)
